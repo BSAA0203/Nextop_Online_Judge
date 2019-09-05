80118d3257cfe1914832a846da36695464966079
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-09-05 13:47:01 +0900

    Update Join.js

--- public/Join.js
+++ public/Join.js
@@ -1,5 +1,5 @@
 (function() {
-  const firebaseConfig = {
+  var firebaseConfig = {
     apiKey: "AIzaSyCaoF4EMIixe1Jp0dWV5RM_UMacV46WML4",
     authDomain: "nextoponlinejudge-52748.firebaseapp.com",
     databaseURL: "https://nextoponlinejudge-52748.firebaseio.com",
@@ -7,19 +7,21 @@
     storageBucket: "",
     messagingSenderId: "600733822228",
     appId: "1:600733822228:web:1cc9343fdf7b802c"
-  };firebase.initializeApp(firebaseConfig);
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
   const txtEmail = document.getElementById('login_field');
   const txtPass = document.getElementById('password');
   const checkBtn = document.getElementById('commit');
   const checkLink = document.getElementById('join');
 
-  checkLink.addEventListener('click',e=>{
+  checkLink.addEventListener('click', e => {
     const email = txtEmail.value;
     const pass = txtPass.value;
     const auth = firebase.auth();
 
-    const promise = auth.signInWithEmailAndPassword(email,pass);
-    promise.catch(e=>console.log(e.message));
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
   });
 }());
